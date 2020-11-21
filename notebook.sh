cf9fa6337f8ca1eb2d681858a6edfa1b9c148563
boonleng <boonleng@ou.edu>
2020-11-20 18:34:54 -0600

    Added transaction counts

--- notebook.sh
+++ notebook.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
+export GOOGLE_APPLICATION_CREDENTIALS="/Users/boonleng/Downloads/bitcoin-analytics-00278a2332d3.json"
+
 if [ "${HOSTNAME}" == "tiffany" ]; then
 	python3 -m notebook --no-browser --port=8081
-elif [ "${HOSTNAME}" == "bumblebee.arrc.ou.edu" ]; then
-	python -m notebook --no-browser --port=8080
-elif [[ "${HOSTNAME}" == "schooner"* ]]; then
-	module load Python/3.8.0-GCCcore-8.2.0
-	module load jupyterlab/1.2.5-foss-2019a-Python-3.8.0
-	module load TensorFlow/2.0.0-foss-2019a-Python-3.8.0
-	python -m notebook --no-browser --port=8080
 else
 	python -m notebook
 fi
